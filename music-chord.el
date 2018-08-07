3852187c7eb74db74f093f5c2a2f79d6781a6573
wouter bolsterlee <wouter@bolsterl.ee>
2018-08-07 20:26:25 +0200

    add some faces

--- music-chord.el
+++ music-chord.el
@@ -1,62 +1,95 @@
+;;; music-chord --- major mode for text files containing musical chords
+
+;;; Commentary:
+
+;; todo
+
+;;; Code:
+
 (require 'rx)
 
 ;; todo defvar instead of setq
-;; todo define faces inheriting from font-lock-*-face
 
-(setq music-chord--font-lock-keywords
-      `((,(rx word-boundary
-              ;; root note
-              (any "ABCDEFG") (? (any "b♭#♯"))
+(defgroup music-chord nil
+  "Text with music chords"
+  :group 'text
+  :prefix "music-chord-")
 
-              ;; chord quality
-              (? (or "Maj" "M" "maj" "min" "m"))
-              (? (or "+" "aug" "0" "dim"))
+(defface music-chord-chord-face
+  '((t :inherit font-lock-constant-face))
+  "Face for chords in music-chord."
+  :group 'music-chord)
+(defvar music-chord-chord-face 'music-chord-chord-face)
 
-              ;; additions
-              (*
-               (? (any "(/"))
-               (? (or "add" "no" "M" "Maj" "maj"))
-               (? (any "b♭#♯+"))
-               (or "2" "3" "5" "6" "7" "9" "11" "13")
-               (? ")")
-               )
+(defface music-chord-header-face
+  '((t :inherit font-lock-comment-face))
+  "Face for hearders in music-chord."
+  :group 'music-chord)
+(defvar music-chord-header-face 'music-chord-header-face)
 
-              ;; suspended chord
-              (? (or "sus2" "sus4"))
+(defface music-chord-repetition-face
+  '((t :inherit font-lock-warning-face))
+  "Face for repetition marks in music-chord."
+  :group 'music-chord)
+(defvar music-chord-repetition-face 'music-chord-repetition-face)
 
-              ;; alternative bass note
-              (? "/" (any "ABCDEFG") (? (any "b♭#♯")))
-              word-boundary)
-         . font-lock-constant-face)
+(defconst music-chord--font-lock-keywords
+  `((,(rx symbol-start
+          ;; root note
+          (any "ABCDEFG") (? (any "b♭#♯"))
 
-        ;; header lines, e.g. [verse]
-        (,(rx
-           bol
-           (any "([")
-           (1+ print)
-           (any ")]")
-           eol)
-         . font-lock-comment-face)
+          ;; chord quality
+          (? (or "Maj" "M" "maj" "min" "m"))
+          (? (or "+" "aug" "0" "dim"))
 
-        ;; repetition marks, e.g. 4x or ×2
-        (,(rx
-           (1+ digit)
-           (any "Xx×"))
-         . font-lock-warning-face)
-        (,(rx
-           (any "Xx×")
-           (1+ digit))
-         . font-lock-warning-face)
-        ))
+          ;; additions
+          (*
+           (? (any "(/"))
+           (? (or "add" "no" "M" "Maj" "maj"))
+           (? (any "b♭#♯+"))
+           (or "2" "3" "5" "6" "7" "9" "11" "13")
+           (? ")")
+           )
+
+          ;; suspended chord
+          (? (or "sus2" "sus4"))
+
+          ;; alternative bass note
+          (? "/" (any "ABCDEFG") (? (any "b♭#♯")))
+          symbol-end)
+     . music-chord-chord-face)
+
+    ;; header lines, e.g. [verse]
+    (,(rx
+       bol
+       (any "([")
+       (1+ print)
+       (any ")]")
+       eol)
+     . music-chord-header-face)
+
+    ;; repetition marks, e.g. 4x or ×2
+    (,(rx
+       (1+ digit)
+       (any "Xx×"))
+     . music-chord-repetition-face)
+    (,(rx
+       (any "Xx×")
+       (1+ digit))
+     . music-chord-repetition-face))
+  "Font lock keywords for music-chord mode.")
 
 (setq music-chord-mode-syntax-table
       (let ((table (make-syntax-table)))
-        (modify-syntax-entry ?\( "w" table)
-        (modify-syntax-entry ?\) "w" table)
-        (modify-syntax-entry ?/ "w" table)
-        (modify-syntax-entry ?+ "w" table)
+        (modify-syntax-entry ?\( "_" table)
+        (modify-syntax-entry ?\) "_" table)
+        (modify-syntax-entry ?/ "_" table)
+        (modify-syntax-entry ?+ "_" table)
         table))
 
 (define-derived-mode music-chord-mode text-mode "music-chord"
   "Major mode for editing text files with musical chords in them."
-  (setq font-lock-defaults '(music-chord--font-lock-keywords)))
+  (setq font-lock-defaults '((music-chord--font-lock-keywords) nil nil)))
+
+(provide 'music-chord)
+;;; music-chord.el ends here
